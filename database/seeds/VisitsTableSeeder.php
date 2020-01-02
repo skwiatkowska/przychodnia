27ab25350d7a0aafd289f718a7d7e3dd887cb99d
Klaudia Rychter <rychter.kl@gmail.com>
2020-01-02 21:50:38 +0100

    fev more visits in seeder

--- database/seeds/VisitsTableSeeder.php
+++ database/seeds/VisitsTableSeeder.php
@@ -26,6 +26,34 @@ class VisitsTableSeeder extends Seeder
                 'godzina_minuta'=>'19:00',
                 'opis'=>'opis wizyty test 2',
                 'zalecenia'=>'zalecenia2',
+            ], [
+                'id_lekarza'=> 1,
+                'id_pacjenta'=> 12,
+                'rok_miesiac_dzien'=>20191228,
+                'godzina_minuta'=>'19:00',
+                'opis'=>'opis wizyty test 3',
+                'zalecenia'=>'zalecenia3',
+            ], [
+                'id_lekarza'=> 2,
+                'id_pacjenta'=> 5,
+                'rok_miesiac_dzien'=>20191229,
+                'godzina_minuta'=>'12:00',
+                'opis'=>'opis wizyty test 4',
+                'zalecenia'=>'zalecenia4',
+            ], [
+                'id_lekarza'=> 3,
+                'id_pacjenta'=> 5,
+                'rok_miesiac_dzien'=>20191222,
+                'godzina_minuta'=>'16:00',
+                'opis'=>'opis wizyty test 6',
+                'zalecenia'=>'zalecenia6',
+            ], [
+                'id_lekarza'=> 3,
+                'id_pacjenta'=> 7,
+                'rok_miesiac_dzien'=>20191221,
+                'godzina_minuta'=>'9:30',
+                'opis'=>'opis wizyty test 5',
+                'zalecenia'=>'zalecenia5',
             ]
         ]);
     }
