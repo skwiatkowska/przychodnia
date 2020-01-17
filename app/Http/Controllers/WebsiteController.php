0e7d280f548bad718783b13fbbabe4c2ded84654
Klaudia Rychter <rychter.kl@gmail.com>
2020-01-17 11:28:53 +0100

    mainSite for different user types

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,12 +3,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\User;
 
 
 class WebsiteController extends Controller
 {
     public function mainSite()
     {
+        $id = Auth::id();
+        if ($id){
+            $user = User::where('id',$id)->first();
+            $typ = $user->user_type;
+            if ($typ=='doctor'){
+                return View('lekarz-panel/panel-lekarza');
+            }else if($typ=='reception'){
+                return View('recepcja-panel/recepcja');
+            }
+        }
+
         return View('home');
     }
 
